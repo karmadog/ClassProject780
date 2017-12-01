f36b565e584d8d6b6df5d3b542b9cf8895328311
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-01 14:33:45 -0600

    Add files via upload

---
+++ src/Factories/NullFactory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Factories;
+
+import data.InputData;
+import data.NullPublication;
+import data.Publication;
+
+/**
+ *
+ * @author karmadog
+ */
+public class NullFactory extends PublicationFactory {
+    
+    InputData parameters = new InputData();
+    
+    boolean isNull(){
+        return true;
+    }
+    
+    @Override
+    public Publication getPublication(InputData parameters){
+        return new NullPublication(parameters);
+    }
+    
+}

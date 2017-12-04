51e82cfad77f6ff7f47d616b94310ab74ed54c97
nimishaniyogi <33362191+nimishaniyogi@users.noreply.github.com>
2017-12-04 10:34:33 -0600

    Update AlphabeticalComparator.java

--- src/data/AlphabeticalComparator.java
+++ src/data/AlphabeticalComparator.java
@@ -1,11 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
-
-// Extract Refactoring from library class
 import java.util.Comparator;
 
 /**

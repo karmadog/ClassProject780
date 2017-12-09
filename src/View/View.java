0ea4dc53b1ff257b025b51aee871686a5c554cad
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:37:17 -0600

    Update View.java
    Remove comments

--- src/View/View.java
+++ src/View/View.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package View;
 
 import LibraryActions.LibraryBehaviour;
@@ -19,10 +15,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-/**
- *
- * @author karma
- */
+
 public class View extends Menu {
     
 

5b82ad05dc8cdc07118b1467966f696243c25b66
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:26:20 -0600

    Update ArticleFactory.java
    Remove comments

--- src/Factories/ArticleFactory.java
+++ src/Factories/ArticleFactory.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Factories;
 
 import data.Article;
 import data.InputData;
 import data.Publication;
 
-/**
- *
- * @author karmadog
- */
+
 public class ArticleFactory extends PublicationFactory{
     
     @Override

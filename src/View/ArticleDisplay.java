11a0dcc7f4918e09f8a2eecca24cbb3cb084be29
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:30:56 -0600

    Update ArticleDisplay.java
    Remove comments

--- src/View/ArticleDisplay.java
+++ src/View/ArticleDisplay.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package View;
 
 
@@ -20,10 +16,7 @@ import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-/**
- *
- * @author karma
- */
+
 public class ArticleDisplay extends Display{
     
     private final JButton exit;

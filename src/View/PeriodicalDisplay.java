bc6d9ad135bca03e365927ba1f65a466809c4dd5
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:36:25 -0600

    Update PeriodicalDisplay.java
    Remove comments

--- src/View/PeriodicalDisplay.java
+++ src/View/PeriodicalDisplay.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package View;
 
 
@@ -21,10 +17,7 @@ import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-/**
- *
- * @author karma
- */
+
 public class PeriodicalDisplay extends Display{
     
     private final JButton exit;

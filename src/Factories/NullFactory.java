00e7062aaa42879a21f514e6a5cd6d17c7c5f880
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:27:10 -0600

    Update NullFactory.java
    Remove comments

--- src/Factories/NullFactory.java
+++ src/Factories/NullFactory.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Factories;
 
 import data.InputData;
 import data.NullPublication;
 import data.Publication;
 
-/**
- *
- * @author karmadog
- */
 public class NullFactory extends PublicationFactory {
     
     boolean isNull(){

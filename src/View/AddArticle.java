4627ba1cc8a8614055ab24add7274348cca74c19
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:28:53 -0600

    Update AddArticle.java
    Remove comments

--- src/View/AddArticle.java
+++ src/View/AddArticle.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package View;
 
 import Factories.ArticleFactory;
@@ -19,10 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-/**
- *
- * @author karma
- */
+
 public class AddArticle extends Menu {
     
     private JLabel titleLabel, authorLabel, journalLabel, pagesLabel, volumeLabel, yearLabel;

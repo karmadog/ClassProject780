85a2dabd4fd291b0dc1f49e96742aae71e8cec14
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:34:45 -0600

    Update ManageLibrary.java
    Remove comments

--- src/View/ManageLibrary.java
+++ src/View/ManageLibrary.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package View;
 
 import LibraryActions.AddUserBehaviour;
@@ -16,10 +12,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-/**
- *
- * @author karma
- */
+
 public class ManageLibrary extends Menu {
     
     private final JButton viewUsers, addUser, exit;

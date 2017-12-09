e05edb296a3eaf54fa422152d142dc5d4909e41c
Michelle Thomas <karmadog@users.noreply.github.com>
2017-12-09 12:35:15 -0600

    Update Menu.java
    Remove comments

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package View;
 
 import LibraryActions.LibraryBehaviour;
@@ -10,10 +6,7 @@ import data.Library;
 import data.LibraryUser;
 import javax.swing.JFrame;
 
-/**
- *
- * @author karma
- */
+
 public abstract class Menu extends JFrame{
     
     protected LibraryBehaviour myBehaviour;
